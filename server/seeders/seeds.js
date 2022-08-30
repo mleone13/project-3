02d5f68279cd34db458217286d966841040262f0
poofy <skyang014@gmail.com>
2022-08-29 22:28:18 -0700

    WOW trying to use node fetch from web apis, we have to import it as an npm. its finally working kinda, need to tweak it a bit more

--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -27,11 +27,13 @@
 //         aboutMe: "Double Trouble"
 //     },
 // ]
-
+// const fetch = require('node-fetch');
+const fetch = (...args) =>
+    import('node-fetch').then(({ default: fetch }) => fetch(...args));
 const faker = require('faker');
 const db = require('../config/connection');
 const { User } = require('../models');
-
+const dogApi = "https://dog.ceo/api/breeds/image/random"
 db.once('open', async () => {
     await User.deleteMany({});
     console.log("here in seeds")
@@ -39,18 +41,41 @@ db.once('open', async () => {
     // create user data
     const userData = [];
 
-    for (let i = 0; i < 50; i += 1) {
+    for (let i = 0; i < 10; i += 1) {
+        // const webDog = async () => {
+        //     const fetchedData = await fetch("https://dog.ceo/api/breeds/image/random");
+        //     const data = await fetchedData.json();
+        //     return data.message
+        // };
+
+        function dogphoto() {
+            fetch(dogApi)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log(data.message);
+                    return data.message;
+                })
+                .catch(function (err) {
+                    console.log(err)
+                })
+        }
+
         const username = faker.internet.userName();
         const email = faker.internet.email(username);
         const password = faker.internet.password();
+        const img = dogphoto();
+        const age = faker.mersenne.rand(85, 20);
+        const aboutMe = faker.lorem.lines();
 
-        userData.push({ username, email, password });
+        userData.push({ username, email, password, img, age, aboutMe });
     }
 
     const createdUsers = await User.collection.insertMany(userData);
 
     // create friends
-    for (let i = 0; i < 100; i += 1) {
+    for (let i = 0; i < 10; i += 1) {
         const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
         const { _id: userId } = createdUsers.ops[randomUserIndex];
 
