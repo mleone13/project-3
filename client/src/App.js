b676749a7431b617e4009f818e1b37ec950e439c
dan <danielornelas01@yahoo.com>
2022-08-31 15:03:23 -0700

    tutor help

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-
+// import { QUERY_ME } from './utils/queries'
+// import { useQuery, useMutation, useLazyQuery} from '@apollo/client'
+// import { ADD_USER } from './utils/mutations';
 function App() {
+  // const {loading, data} = useQuery(QUERY_ME)// onpage load
+  // const [addUser, {error}] = useMutation(ADD_USER)// returns a function you can call at anytime
+  // const [queryMe, additionalInfo] = useLazyQuery(QUERY_ME)// returns a function you can call at anytime
+  // addUser("Daniel","daniel@email.com", "123abc", "www.somewhere.com", 23, "Coding is fun")
+  // queryMe()
   return (
     <div className="App">
       <header className="App-header">
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -30,3 +30,5 @@ const typeDefs = gql`
 
 `;
 module.exports = typeDefs;
+
+
