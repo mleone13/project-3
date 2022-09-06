e0026df815fa937f17890a884a4f67ce55a25a92
dan <danielornelas01@yahoo.com>
2022-09-06 02:03:59 -0700

    workking on dashboard

--- client/src/pages/Onboarding.js
+++ client/src/pages/Onboarding.js
@@ -28,7 +28,7 @@ const Onboarding = () => {
         console.log('submitted')
         e.preventDefault()
         try {
-            const response = await axios.put('http://localhost:3000/user', {formData})
+            const response = await axios.put('http://localhost:8000/user', {formData})
             console.log(response)
             const success = response.status === 200
             if (success) navigate('/dashboard')
--- server/server.js
+++ server/server.js
@@ -251,4 +251,61 @@ app.post('/message', async (req, res) => {
 })
 
 
-app.listen(PORT, () => console.log('server running on PORT ' + PORT))
+
+const path = require('path');
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+// import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+
+// create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+});
+
+
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+    await server.start();
+    // integrate our Apollo server with the Express application as middleware
+    server.applyMiddleware({ app });
+
+    // Serve up static assets
+    //this and appget for concurrent
+    //First, we check to see if the Node environment is in production. 
+    //If it is, we instruct the Express.js server to serve any files in the 
+    //React application's build directory in the client folder. We don't have 
+    //a build folder yet—because remember, that's for production only!
+
+    //The next set of functionality we created was a wildcard GET route for 
+    //the server. In other words, if we make a GET request to any location on 
+    //the server that doesn't have an explicit route defined, respond with the 
+    //production-ready React front-end code.
+    if (process.env.NODE_ENV === 'production') {
+        app.use(express.static(path.join(__dirname, '../client/build')));
+    }
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    });
+
+    db.once('open', () => {
+        app.listen(PORT, () => {
+            console.log(`API server running on port ${PORT}!`);
+            // log where we can go to test our GQL API
+            console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+        })
+    })
+};
+
+// Call the async function to start the server
+startApolloServer(typeDefs, resolvers);
